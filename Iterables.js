2a4a95f436761f65f2162d02bef0516de78b139b
Muditkul05 <muditkulsh05@gmail.com>
2026-07-08 10:35:29 +0530

    Implemented a custom made iterator using Symbol.iterator

---
+++ Iterables.js
@@ -0,0 +1,23 @@
+let range = {
+    text : "Hello",
+    currentindex : 0,
+};
+range[Symbol.iterator] = function(){
+    return{
+        currentindex : 0,
+        text : "Hello",
+        current : this.text[this.currentindex],    //current : Hello[0],
+        last : this.text.length,                   //last : 5
+        next(){
+            if(this.currentindex < this.last ){
+                return{done : false , value : this.text[this.currentindex++]};
+                 
+            }else{
+                return{done : true , value : "failed"};
+            }
+        }
+    }
+}
+for(let result of range){
+    console.log(result)
+}
