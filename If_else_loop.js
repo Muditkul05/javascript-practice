57f31506418bd84f6453c0f08c17d8fe0c76ec7b
Muditkul05 <muditkulsh05@gmail.com>
2026-07-26 14:41:09 +0530

    Trying if else loop

--- If_else_loop.js
+++ If_else_loop.js
@@ -1,6 +1,8 @@
-let ans = prompt(`What is the official name of javascript?`, '' )
-if(ans == "ECMAScript"){
-    alert(`Right!!`);
-}else{
-    alert(`U don't know  ? ECMAScript`);
+let ans = prompt("Write a number" , 0);
+if(ans > 0) {
+    alert(1);
+}else if(ans < 0){
+    alert(-1);
+}else {
+    alert(0);
 }
