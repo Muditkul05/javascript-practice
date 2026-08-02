58124ab02a9edf589c94270e9a6a1c3f230cf60d
Muditkul05 <muditkulsh05@gmail.com>
2026-08-02 15:30:18 +0530

    feat: fix camelize function using split join and map methods

--- camelize.js
+++ camelize.js
@@ -1,10 +1,12 @@
 let camelize = (str) => {
    let arr =  str.split('-');
-    arr.map(function item (){
-        arr[0] = arr[0];
-        arr[1] = arr[0].toUpperCase() + arr.slice(1);
-        arr[2] = arr[0].toUpperCase() + arr.slice(1);
-        return arr;
-    })
+   let result = arr.map(function item (word,index , array){
+    if(index == 0){
+        return word;
+    }
+       return word[0].toUpperCase() + word.slice(1);
+    }
+    )
+    return result.join('');
 }
 console.log(camelize("my-short-string"));
