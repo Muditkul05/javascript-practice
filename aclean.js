69e58c94a2847fc95a675efea184522bc99aea05
Muditkul05 <muditkulsh05@gmail.com>
2026-08-06 17:50:00 +0530

    feat: implement aclean function to remove anagrams from array

---
+++ aclean.js
@@ -0,0 +1,10 @@
+let aclean = (arr) => {
+    let map = new Map();
+    for(let word of arr){
+        let sorted = word.toLowerCase().split('').sort().join();
+        map.set(sorted,word);
+    }
+    return Array.from(map.values());
+}
+let arr = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
+console.log(aclean(arr));
